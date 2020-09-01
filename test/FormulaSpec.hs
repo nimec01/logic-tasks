f7ead0909f9879439b03ede94e3f123836cd6540
Patrick Ritzenfeld <patrick.ritzenfeld@stud.uni-due.de>
2020-09-01 16:39:36 +0200

    Fehlerhaften Testfall berichtigt

--- test/FormulaSpec.hs
+++ test/FormulaSpec.hs
@@ -3,12 +3,13 @@ module FormulaSpec where
 import Test.Hspec
 import Test.QuickCheck
 import Formula
+import Control.Exception(evaluate)
 
 
 spec :: Spec
 spec = do
   describe "genLiteral" $ do
     it "should throw an error when called with the empty list" $ do
-      generate (genLiteral []) `shouldThrow` errorCall "Can not construct Literal from empty list."
+      evaluate (genLiteral []) `shouldThrow` errorCall "Can not construct Literal from empty list."
     it "should generate a random literal from the given char list" $ do
       property $ \x -> not (null x) ==> forAll (genLiteral x) $ \y -> getC y `elem` x
