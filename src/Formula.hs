d1c4fefd5f0c40b7da734a30e1a1de7c45a04bdb
Patrick Ritzenfeld <patrick.ritzenfeld@stud.uni-due.de>
2020-09-12 17:28:15 +0200

    Verhalten bei duplizierten Chars in der Eingabeliste gefixt

--- src/Formula.hs
+++ src/Formula.hs
@@ -16,10 +16,10 @@ module Formula
        ) where
 
 
-import Data.List (delete)
-import Data.Set (Set,member,size,fromList,toList,empty,insert,unions)
+import Data.List (delete, nub)
+import Data.Set (Set,empty)
 import Test.QuickCheck
-import qualified Data.Set as Set (map)
+import qualified Data.Set as Set
 
 
 type Allocation = [(Literal, Bool)]
@@ -32,27 +32,31 @@ data Literal
     deriving (Eq,Ord)
 
 instance Show Literal where
- show (Literal x) = [x]
- show (Not x) = "not(" ++ [x] ++ ")"
+   show (Literal x) = [x]
+   show (Not x) = "not(" ++ [x] ++ ")"
 
 instance Read Literal where
-  readsPrec _ ('n':'o':'t':'(':x:')':rest) = [(Not x, rest) | x `elem` ['A' .. 'Z']]
-  readsPrec _ (x:rest) = [(Literal x, rest) | x `elem` ['A' .. 'Z']]
-  readsPrec _ _ = []
+   readsPrec _ ('n':'o':'t':'(':x:')':rest) = [(Not x, rest) | x `elem` ['A' .. 'Z']]
+   readsPrec _ (x:rest) = [(Literal x, rest) | x `elem` ['A' .. 'Z']]
+   readsPrec _ _ = []
 
 instance Arbitrary Literal where
-  arbitrary = genLiteral ['A'..'Z']
+   arbitrary = genLiteral ['A'..'Z']
+
+
 
 evalLiteral :: Allocation -> Literal -> Maybe Bool
 evalLiteral xs (Not y) = not <$> evalLiteral xs (Literal y)
 evalLiteral xs z = lookup z xs
 
 
+
 genLiteral :: [Char] -> Gen Literal
 genLiteral [] = error "Can not construct Literal from empty list."
 genLiteral lits = do
- rChar <- elements lits
- elements [Literal rChar, Not rChar]
+   rChar <- elements lits
+   elements [Literal rChar, Not rChar]
+
 
 
 opposite :: Literal -> Literal
@@ -71,36 +75,43 @@ newtype Clause = Clause { getLs :: Set Literal}
     deriving (Eq,Ord)
 
 instance Show Clause where
- show (Clause set) = listShow (toList set)
-
-  where listShow [] = "False"
-        listShow [x] = show x
-        listShow (x:xs) = show x ++ " OR " ++ listShow xs
+   show (Clause set) = listShow (Set.toList set)
+     where
+       listShow [] = "False"
+       listShow [x] = show x
+       listShow (x:xs) = show x ++ " OR " ++ listShow xs
 
 instance Arbitrary Clause where
-  arbitrary = sized clause
-    where clause 0 = genClause (0,0) []
-          clause n = genClause (1,maxBound) (take n ['A'..'Z'])
+   arbitrary = sized clause
+     where
+       clause 0 = genClause (0,0) []
+       clause n = genClause (1,maxBound) (take n ['A'..'Z'])
 
 
 
 evalClause :: Allocation -> Clause -> Maybe Bool
 evalClause xs ys = or <$> sequence literals
- where literals = map (evalLiteral xs) (toList (getLs ys))
+  where
+    literals = map (evalLiteral xs) (Set.toList (getLs ys))
+
 
 
 genClause :: (Int,Int) -> [Char] -> Gen Clause
 genClause (minlen,maxlen) lits
- | null lits || minlen <= 0 || minlen > length lits || minlen > maxlen = return (Clause empty)
- | otherwise = do
-  len <- chooseInt (minlen,minimum [length lits, maxlen])
-  literals <- generateLiterals lits [] len
-  return (Clause (fromList literals))
-   where generateLiterals usedLits xs len
-           | length xs == len = return xs
-           | otherwise = do
-              literal <- genLiteral usedLits
-              generateLiterals (delete (getC literal) usedLits) (literal:xs) len
+    | null nLits || minlen > length lits || invalidLen = pure (Clause empty)
+    | otherwise = do
+        len <- chooseInt (minlen,minimum [length nLits, maxlen])
+        literals <- generateLiterals nLits [] len
+        pure (Clause (Set.fromList literals))
+  where
+    nLits = nub lits
+    invalidLen = minlen > maxlen || minlen <= 0
+
+    generateLiterals usedLits xs len
+        | length xs == len = pure xs
+        | otherwise = do
+            literal <- genLiteral usedLits
+            generateLiterals (delete (getC literal) usedLits) (literal:xs) len
 
 
 
@@ -109,10 +120,13 @@ genClause (minlen,maxlen) lits
 newtype Cnf = Cnf { getCs :: Set Clause}
      deriving (Eq,Ord)
 
+
+
 instance Arbitrary Cnf where
-   arbitrary = sized cnf
-    where cnf 0 = genCnf (0,0) (0,0) []
-          cnf n = do
+    arbitrary = sized cnf
+      where
+        cnf 0 = genCnf (0,0) (0,0) []
+        cnf n = do
             minLen <- chooseInt (1,n)
             let lits = take n ['A'..'Z']
             let maxLen = length lits
@@ -121,36 +135,46 @@ instance Arbitrary Cnf where
 
 
 instance Show Cnf where
- show (Cnf set) = listShow (toList set)
+    show (Cnf set) = listShow (Set.toList set)
+      where
+        listShow [] = "True"
+        listShow [x] = show x
+        listShow (x:xs) = "(" ++ show x ++ ") AND (" ++ listShow xs ++ ")"
 
-   where listShow [] = "True"
-         listShow [x] = show x
-         listShow (x:xs) = "(" ++ show x ++ ") AND (" ++ listShow xs ++ ")"
 
 
 evalCnf :: Allocation -> Cnf -> Maybe Bool
 evalCnf xs ys = and <$> sequence clauses
- where clauses = map (evalClause xs) (toList (getCs ys))
+  where clauses = map (evalClause xs) (Set.toList (getCs ys))
 
 
 
 getLiterals :: Cnf -> [Literal]
-getLiterals cnf = toList $ unions $ map (Set.map turnPositive . getLs) $ toList (getCs cnf)
+getLiterals cnf = Set.toList $ Set.unions $ map positive $ Set.toList (getCs cnf)
+  where positive = Set.map turnPositive . getLs
 
 
 
 genCnf :: (Int,Int) -> (Int,Int) -> [Char] -> Gen Cnf
 genCnf (minNum,maxNum) (minLen,maxLen) lits
- | null lits || minLen <= 0 || minLen > length lits
-   || minLen > maxLen || minNum <= 0 || minNum > maxNum
-   || minNum > upperBound = return (Cnf empty)
- | otherwise = do
-  num <- chooseInt (minNum, minimum [maxNum,upperBound])
-  cnf <- generateClauses lits empty num
-  return (Cnf cnf)
-   where upperBound = minimum [2^maxLen, 2^length lits]
-         generateClauses usedLits set num
-            | size set == num = return set
-            | otherwise = do
-               clause <- genClause (minLen,maxLen) usedLits
-               generateClauses usedLits (if clause `member` set then set else insert clause set) num
+    | null nLits || invalidLen || invalidNum = pure (Cnf empty)
+    | otherwise = do
+        num <- chooseInt (minNum, minimum [maxNum,upperBound])
+        cnf <- generateClauses nLits empty num
+        pure (Cnf cnf)
+  where
+    nLits = nub lits
+    invalidLen = minLen <= 0 || minLen > maxLen || minLen > length nLits
+    invalidNum = minNum <= 0 || minNum > maxNum || minNum > upperBound
+    upperBound = minimum [2^maxLen, 2^length nLits]
+
+    generateClauses usedLits set num
+        | Set.size set == num = return set
+        | otherwise = do
+            clause <- genClause (minLen,maxLen) usedLits
+            generateClauses usedLits (decide clause) num
+      where
+        alreadyIn = (flip Set.member) set
+        decide c
+            | alreadyIn c = set
+            | otherwise = Set.insert c set
