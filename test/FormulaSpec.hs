1eb54ba7dab52e54ed951c7af07deb1ee7b650bc
Patrick Ritzenfeld <patrick.ritzenfeld@stud.uni-due.de>
2020-09-12 17:15:26 +0200

    Fehler in Testfall korrigiert

--- test/FormulaSpec.hs
+++ test/FormulaSpec.hs
@@ -1,41 +1,50 @@
 module FormulaSpec where
 
+
+
 import Test.Hspec
 import Test.QuickCheck
 import Formula
 import Control.Exception(evaluate)
-import Data.Set (toList,fromList,empty,member,size,findMax,findMin)
-import qualified Data.Set as Set (map)
+import qualified Data.Set as Set
+import Data.Set (empty,Set)
 
 
 
 allocGen :: Gen Allocation
 allocGen = do
- alloc <- arbitrary
- let flipped = flipNegs alloc
- return flipped
-  where flipNegs = map (\x -> let first = fst x in if negative first
-                                                     then (opposite first,not (snd x))
-                                                     else x)
-        negative (Not _) = True
-        negative _ = False
+    alloc <- arbitrary
+    let flipped = flipNegs alloc
+    pure flipped
+  where
+    flipNegs = map flipper
+    flipper x
+        | negative first = (opposite first,not (snd x))
+        | otherwise      = x
+      where
+        first = fst x
+    negative (Not _) = True
+    negative _ = False
+
 
 
 validBoundsClause :: Gen ((Int,Int),[Char])
 validBoundsClause = do
- validChars <- sublistOf ['A'..'Z']
- let upperBound = length validChars
- lower <- chooseInt (1,upperBound)
- upper <- chooseInt (lower,upperBound)
- return ((lower,upper),validChars)
+    validChars <- sublistOf ['A'..'Z']
+    let upperBound = length validChars
+    lower <- chooseInt (1,upperBound)
+    upper <- chooseInt (lower,upperBound)
+    pure ((lower,upper),validChars)
+
+
 
 validBoundsCnf :: Gen ((Int,Int),(Int,Int),[Char])
 validBoundsCnf = do
- ((minLen,maxLen),chars) <- validBoundsClause
- let upperBound = minimum [2^ maxLen, 2^length chars]
- minNum <- chooseInt (1,upperBound)
- maxNum <- chooseInt (minNum,upperBound)
- return ((minNum,maxNum),(minLen,maxLen),chars)
+    ((minLen,maxLen),chars) <- validBoundsClause
+    let upperBound = minimum [2^ maxLen, 2^length chars]
+    minNum <- chooseInt (1,upperBound)
+    maxNum <- chooseInt (minNum,upperBound)
+    pure ((minNum,maxNum),(minLen,maxLen),chars)
 
 
 
@@ -65,14 +74,14 @@ spec = do
                    ==> forAll (genClause (lower,upper) lits) (== Clause empty)
     it "should generate a random clause of the correct length if given valid parameters" $
       forAll validBoundsClause $ \((lower,upper),chars) -> forAll (genClause (lower,upper) chars) $ \clause ->
-        let len = size (getLs clause) in len >= lower && len <= upper
+        let len = Set.size (getLs clause) in len >= lower && len <= upper
 
   describe "evalClause" $ do
     it "should return Nothing when called with an empty allocation" $
       property $ \clause -> clause /= Clause empty ==> evalClause [] clause `shouldBe` Nothing
     it "should return Nothing when called with a mismatched allocation" $
       property $ \clause -> forAll allocGen $
-        \alloc -> not (all (`elem` map fst alloc) (map turnPositive (toList (getLs clause))))
+        \alloc -> not (all (`elem` map fst alloc) (map turnPositive (Set.toList (getLs clause))))
           ==> evalClause alloc clause `shouldBe` Nothing
 
 
@@ -81,11 +90,11 @@ spec = do
       property $ \bounds1 bounds2 -> forAll (genCnf bounds1 bounds2 []) (== Cnf empty)
     it "should return the empty conjunction when called with invalid boundaries" $
       property $ \lower1 upper1 lower2 upper2 lits -> lower1 <= 0 || lower2 <= 0
-                 || upper1 < lower1 || upper2 < lower2 || upper1 > minimum [2^lower2, 2^length lits]
+                 || upper1 < lower1 || upper2 < lower2 || lower1 > minimum [2^lower2, 2^length lits]
                    ==> forAll (genCnf (lower1,upper1) (lower2,upper2) lits) (== Cnf empty)
     it "should generate a random cnf formula with a correct amount of clauses if given valid parameters" $
       forAll validBoundsCnf $ \((lowerNum,upperNum),(lowerLen,upperLen),chars) -> forAll (genCnf (lowerNum,upperNum) (lowerLen,upperLen) chars) $ \cnf ->
-        let num = size (getCs cnf) in num >= lowerNum && num <= upperNum
+        let num = Set.size (getCs cnf) in num >= lowerNum && num <= upperNum
     it "should generate a random cnf formula with the correct clause length if given valid parameters" $
       forAll validBoundsCnf $ \((lowerNum,upperNum),(lowerLen,upperLen),chars) -> forAll (genCnf (lowerNum,upperNum) (lowerLen,upperLen) chars) $ \cnf ->
-       let sizes = Set.map size (Set.map getLs (getCs cnf)) in findMax sizes <= upperLen && findMin sizes >= lowerLen
+       let sizes = Set.map Set.size (Set.map getLs (getCs cnf)) in Set.findMax sizes <= upperLen && Set.findMin sizes >= lowerLen
