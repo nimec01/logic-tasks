13c80df971a33bac8e9d9960e86f34ebffbb9766
Patrick Ritzenfeld <patrick.ritzenfeld@stud.uni-due.de>
2021-03-12 15:35:42 +0100

    Add interfaces for SAT-Solver

--- src/Formula.hs
+++ src/Formula.hs
@@ -1,16 +1,23 @@
+
 module Formula
        (
-         mkClause
+         turnPositive
+       , isPositive
+       , mkClause
        , isEmptyClause
        , mkCnf
        , isEmptyCnf
        , hasEmptyClause
-       , turnPositive
-       , isPositive
+       , xorSat
+       , orSat
+       , andSat
+       , implSat
+       , equivSat
        ) where
 
 
 import qualified Data.Set as Set
+import qualified SAT.MiniSat as Sat
 
 import Types
 
@@ -47,3 +54,30 @@ isEmptyCnf (Cnf set) = Set.null set
 
 hasEmptyClause :: Cnf -> Bool
 hasEmptyClause (Cnf set) = Clause Set.empty `Set.member` set
+
+
+
+---------------------------------------------------------------------------------------------------
+
+
+xorSat :: (Formula a, Formula b) => a -> b -> Bool
+xorSat f1 f2 = Sat.satisfiable (convert f1 Sat.:++: convert f2)
+
+
+
+andSat :: (Formula a, Formula b) => a -> b -> Bool
+andSat f1 f2 = Sat.satisfiable (convert f1 Sat.:&&: convert f2)
+
+
+orSat :: (Formula a, Formula b) => a -> b -> Bool
+orSat f1 f2 = Sat.satisfiable (convert f1 Sat.:||: convert f2)
+
+
+implSat :: (Formula a, Formula b) => a -> b -> Bool
+implSat f1 f2 = Sat.satisfiable (convert f1 Sat.:->: convert f2)
+
+
+equivSat :: (Formula a, Formula b) => a -> b -> Bool
+equivSat f1 f2 = Sat.satisfiable (convert f1 Sat.:<->: convert f2)
+
+
