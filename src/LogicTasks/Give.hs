8a737418b595a1ea57151597948ef34fe8284ca0
Patrick Ritzenfeld <patrick.ritzenfeld@stud.uni-due.de>
2021-10-01 13:14:50 +0200

    Edit copy pasted description line to fix table header for max term exercise.

--- src/LogicTasks/Give.hs
+++ src/LogicTasks/Give.hs
@@ -26,7 +26,7 @@ description GiveInst{..} =
                      ,"Consider the following truth table:"
                      )
               , PDoc line
-              , PDoc $ nest 4 $ myText "F = " <+> pretty (getTable cnf)
+              , PDoc $ nest 4 $ pretty (getTable cnf)
               , PDoc line
               , PMult ("Geben Sie eine zu der Tafel passende Formel in konjunktiver Normalform an. Verwenden Sie dazu Max-Terme."
                      ,"Provide a formula in conjunctive normal form, that corresponds to the table. Use maxterms to do this."
