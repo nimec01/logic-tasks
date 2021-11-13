63dc0bcf7f343ce3f08f14077c8c05639e6a81a3
Patrick Ritzenfeld <patrick.ritzenfeld@stud.uni-due.de>
2021-11-13 13:24:16 +0100

    initial draft for dnf formulae

--- src/Types.hs
+++ src/Types.hs
@@ -1,4 +1,5 @@
 {-# language DeriveGeneric #-}
+{-# language DuplicateRecordFields #-}
 
 -- | Some basic types for propositional logic
 module Types
@@ -7,7 +8,9 @@ module Types
        , Literal(..)
        , opposite
        , Clause(..)
+       , Disj(..)
        , Cnf(..)
+       , Dnf(..)
        , getClauses
        , Table(..)
        , getTable
@@ -334,6 +337,224 @@ genCnf (minNum,maxNum) (minLen,maxLen) lits
             clause <- genClause (minLen,maxLen) usedLits
             generateClauses usedLits (Set.insert clause set) num
 
+
+
+
+
+------------------------------------------------------------
+
+-- | A datatype representing a disjunction
+newtype Disj = Disj { literalSet :: Set Literal}
+    deriving (Eq,Typeable,Generic)
+
+
+
+instance Ord Disj where
+   compare (Disj set1) (Disj set2)
+       | size1 /= size2 = compare size1 size2
+       | otherwise = compare set1 set2
+     where
+       size1 = Set.size set1
+       size2 = Set.size set2
+
+
+
+instance Show Disj where
+   show (Disj set) = listShow (Set.toList set)
+     where
+       listShow :: [Literal] -> String
+       listShow [] = "True"
+       listShow [x] = show x
+       listShow (x:xs) = show x ++ " AND " ++ listShow xs
+
+
+
+instance Formula Disj where
+   convert (Disj set)
+        | Set.null set = Sat.Yes
+        | otherwise = Sat.All (map convert (Set.toList set))
+
+   literals (Disj set) = Set.toList set
+
+   atomics (Disj set) = concat $ Set.toList $ Set.map atomics set
+
+   amount (Disj set) = Set.size set
+
+   evaluate xs ys = and <$> sequence lits
+     where
+       lits = map (evaluate xs) (literals ys)
+
+
+
+
+
+partEvalDisj :: Disj -> (Literal,Bool) -> Either Bool Disj
+partEvalDisj (Disj set) x
+    | Set.null set = Left True
+    | isIn || negIsIn =
+     if snd x then if isIn then Right (Disj setWithout) else Right (Disj set)
+              else if isIn then Left False else Right (Disj setWithoutNeg)
+    | otherwise = Right (Disj set)
+  where
+    next = fst x
+    negNext = opposite next
+    isIn = next `Set.member` set
+    negIsIn = negNext `Set.member` set
+    setWithout = Set.delete next set
+    setWithoutNeg = Set.delete negNext set
+
+
+
+instance Arbitrary Disj where
+   arbitrary = sized disj
+     where
+       disj :: Int -> Gen Disj
+       disj 0 = genDisj (0,0) []
+       disj n = genDisj (1,maxBound) (take n ['A'..'Z'])
+
+
+-- | Generates a random disjunction. The length of the generated disjunction lies in the given length bounds.
+--   The used atomic formulae are drawn from the list of chars.
+genDisj :: (Int,Int) -> [Char] -> Gen Disj
+genDisj (minlen,maxlen) lits
+    | null lits || minlen > length nLits || invalidLen = pure (Disj empty)
+    | otherwise = do
+        len <- choose (minlen,minimum [length nLits, maxlen])
+        genLits <- generateLiterals nLits empty len
+        pure (Disj genLits)
+  where
+    nLits = nub lits
+    invalidLen = minlen > maxlen || minlen <= 0
+
+    generateLiterals :: [Char] -> Set Literal -> Int -> Gen (Set Literal)
+    generateLiterals usedLits xs len
+        | Set.size xs == len = pure xs
+        | otherwise = do
+            literal <- genLiteral usedLits
+            let
+              restLits = delete (letter literal) usedLits
+              newSet = Set.insert literal xs
+            generateLiterals restLits newSet len
+
+
+
+--------------------------------------------------------------
+
+-- | A datatype representing a formula in disjunctive normalform.
+newtype Dnf = Dnf { clauseSet :: Set Disj}
+     deriving (Eq,Typeable,Generic)
+
+
+
+instance Ord Dnf where
+   compare (Dnf set1) (Dnf set2)
+       | size1 /= size2 = compare size1 size2
+       | otherwise = compare set1 set2
+     where
+       size1 = Set.size set1
+       size2 = Set.size set2
+
+
+
+
+-- | Retrieves the contained conjunctions of the dnf.
+getDisjunctions :: Dnf -> [Disj]
+getDisjunctions (Dnf set) = Set.toList set
+
+
+
+
+partEvalDnf :: Dnf -> (Literal,Bool) -> Either Bool Dnf
+partEvalDnf dnf tup
+    | fst tup `notElem` lits = Right dnf
+    | otherwise = result (thin applied)
+  where
+    lits = literals dnf
+    disjs = getDisjunctions dnf
+    applied = map (`partEvalDisj` tup) disjs
+    thin :: [Either Bool Disj] -> [Either Bool Disj]
+    thin [] = []
+    thin (x:xs) =
+      case x of Left False   -> thin xs
+                Left True    -> [Left True]
+                Right disj -> Right disj : thin xs
+    result :: [Either Bool Disj] -> Either Bool Dnf
+    result xs
+      | null xs = Left False
+      | Left True `elem` xs = Left True
+      | otherwise = Right (Dnf (Set.fromList (rights xs)))
+
+
+instance Show Dnf where
+    show (Dnf set) = listShow (Set.toList set)
+      where
+        listShow :: [Disj] -> String
+        listShow [] = "False"
+        listShow [x] = show x
+        listShow (x:xs) = "(" ++ show x ++ ") OR (" ++ listShow xs ++ ")"
+
+
+instance Formula Dnf where
+    convert (Dnf set)
+        | Set.null set = Sat.No
+        | otherwise = Sat.Some (map convert (Set.toList set))
+
+    literals (Dnf set) = Set.toList $ Set.unions $ Set.map (Set.fromList . literals) set
+
+    atomics (Dnf set) = Set.toList $ Set.unions $ Set.map (Set.fromList . atomics) set
+
+    amount (Dnf set) = Set.size set
+
+    evaluate alloc dnf = or <$> sequence disjs
+      where
+        disjs = map (evaluate alloc) (getDisjunctions dnf)
+
+
+
+instance Arbitrary Dnf where
+    arbitrary = sized dnf
+      where
+        dnf :: Int -> Gen Dnf
+        dnf 0 = genDnf (0,0) (0,0) []
+        dnf n = do
+            minLen <- choose (1,n)
+            let
+              lits = take n ['A'..'Z']
+              maxLen = length lits
+            genDnf (1,maxLen ^2) (minLen,maxLen) lits
+
+
+
+
+-- | Generates a random dnf satisfying the given bounds
+--   for the amount and the length of the contained disjunctions.
+--   The used atomic formulae are drawn from the list of chars.
+
+genDnf :: (Int,Int) -> (Int,Int) -> [Char] -> Gen Dnf
+genDnf (minNum,maxNum) (minLen,maxLen) lits
+    | null nLits || invalidLen || invalidNum = pure (Dnf empty)
+    | otherwise = do
+        num <- choose (minNum, minimum [maxNum,upperBound])
+        dnf <- generateDisjs nLits empty num
+        pure (Dnf dnf)
+  where
+    nLits = nub lits
+    invalidLen = minLen <= 0 || minLen > maxLen || minLen > length nLits
+    invalidNum = minNum <= 0 || minNum > maxNum || minNum > upperBound
+    lengthBound 1 len = 2*len
+    lengthBound n len
+        | n == maxLen && n == minLen = 2^n
+        | n == minLen = 2^n * len
+        | n == len = 2^n + lengthBound (n-1) len
+        | otherwise = 2^n * len + lengthBound (n-1) len
+    upperBound = lengthBound maxLen (length nLits)
+
+    generateDisjs :: [Char] -> Set Disj -> Int -> Gen (Set Disj)
+    generateDisjs usedLits set num
+        | Set.size set == num = pure set
+        | otherwise = do
+            disj <- genDisj (minLen,maxLen) usedLits
+            generateDisjs usedLits (Set.insert disj set) num
 
 
 ------------------------------------------------------------
