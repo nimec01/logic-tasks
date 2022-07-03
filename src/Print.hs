c5dba1cac8b77f75003d33bc07788a30a8210907
xutongxi <49649765+xutongxi@users.noreply.github.com>
2022-07-03 15:06:35 +0200

    Print.hs show the String form which can be used in\usepackage[linguistics]{forest} to show a Syntax tree

--- src/Print.hs
+++ src/Print.hs
@@ -1,14 +1,26 @@
 module Print(
+    transfer,
     main
     ) where
--- import Types
+import Types
 -- import Parsing
--- import Test.QuickCheck
+import Test.QuickCheck
+import Data.Maybe
 
 -- prop_syntree :: SynTree  -> Bool
 -- prop_syntree a = normParse ( show a) == Right  a
 
 -- prop_add :: Int ->Bool
 -- prop_add a =(a+1)==(1+a)
+transfer :: SynTree ->String
+transfer (And a b) = "[ $\\wedge $ " ++ transfer a++ transfer b++"  ]"
+transfer (Leaf a)= "[" ++ (a:"]")
+transfer (Or a b) = "[ $\\vee   $ " ++ transfer a++ transfer b++"  ]"
+transfer (Not a) = "[ $\\neg $ " ++ transfer a ++"  ]"
+transfer (Impl a b) = "[ $\\to  $ " ++ transfer a++ transfer b++"  ]"
+transfer (Equi a b) = "[ $\\Leftrightarrow  $ " ++ transfer a++ transfer b++"  ]"
+
 main :: IO ()
-main = undefined
+main = do
+    tree <- generate $ fromJust $genSynTree ((4, 10)) 5 "ABCD" ""
+    print (transfer tree)
