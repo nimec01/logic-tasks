aaeb51043faea447a6e19eb78a7b0a88fe810ea0
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2022-07-05 21:29:31 +0200

    refactored parser, to avoid using "try", while still being close enough to the on-paper-definition

--- src/Parsing.hs
+++ src/Parsing.hs
@@ -1,29 +1,20 @@
 {-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
-{-# LANGUAGE BlockArguments #-}
-module Parsing(
-  normParse
-) where
 
-import Types ( SynTree(Equi, Leaf, Not, And, Or, Impl) )
-import Text.Parsec.String ( Parser )
-import Text.ParserCombinators.Parsec(try)
-import Text.Parsec.Char ( char, oneOf, satisfy, string )
+module Parsing (
+  normParse
+  ) where
+
+import Types (SynTree(..))
+import Text.Parsec.String (Parser)
+import Text.Parsec.Char (char, oneOf, satisfy, string)
 import Control.Applicative ((<|>), many)
 import Data.Char (isLetter)
-import Text.Parsec(ParseError,parse,eof)
+import Text.Parsec (ParseError, parse, eof)
 
-parseWithEof :: Parser a -> String -> Either ParseError a
-parseWithEof p = parse (p <* eof) ""
 whitespace :: Parser ()
 whitespace = do
   many $ oneOf " \n\t"
   return ()
-parseWithWhitespace :: Parser a -> String -> Either ParseError a
-parseWithWhitespace p = parseWithEof wrapper
- where
- wrapper = do
-  whitespace
-  p
 
 lexeme :: Parser a -> Parser a
 lexeme p = do
@@ -32,36 +23,32 @@ lexeme p = do
  return x
 
 leafE :: Parser SynTree
-leafE = do
- a <- lexeme $ satisfy isLetter
- return $ Leaf  a
+leafE =
+ Leaf <$> lexeme (satisfy isLetter)
 
 notE :: Parser SynTree
-notE =  do
+notE = do
  lexeme $ char '~'
  Not <$> parserT
 
-simpleBothE::(Parser String,SynTree -> SynTree -> SynTree) ->Parser SynTree
-simpleBothE (bothparse,oper) = do
- left <- parserT
- lexeme bothparse
- oper left <$> parserT
-
 parserTtoS :: Parser SynTree
-parserTtoS=  do
+parserTtoS = do
  lexeme $ char '('
  e <- parserS
  lexeme $ char ')'
  return e
 
 parserT :: Parser SynTree
-parserT = try leafE <|>try parserTtoS  <|> notE
-
-parserBothT :: Parser SynTree
-parserBothT= (try $ simpleBothE (string "/\\",And) )<|> (try $ simpleBothE (string "\\/",Or))<|>(try $ simpleBothE (string "=>",Impl)) <|> (simpleBothE (string "<=>",Equi))
+parserT = leafE <|> parserTtoS <|> notE
 
 parserS :: Parser SynTree
-parserS = try  parserBothT <|> parserT
+parserS = do
+  firstT <- parserT
+  (lexeme (string "/\\") >> And firstT <$> parserT) <|>
+    (lexeme (string "\\/") >> Or firstT <$> parserT) <|>
+    (lexeme (string "=>") >> Impl firstT <$> parserT) <|>
+    (lexeme (string "<=>") >> Equi firstT <$> parserT) <|>
+    return firstT
 
-normParse :: String ->Either ParseError SynTree
-normParse = parseWithWhitespace parserS
+normParse :: String -> Either ParseError SynTree
+normParse = parse (whitespace >> parserS <* eof) ""
