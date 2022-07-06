2068aab42e6c832351558e7aab1ce88aad9f3b56
xutongxi <49649765+xutongxi@users.noreply.github.com>
2022-07-06 22:25:24 +0200

    use some where and move judge to binaryOperator negativeFormula

--- src/Generate.hs
+++ src/Generate.hs
@@ -32,40 +32,48 @@ genSynTree (minnode, maxnode) maxdepth lits minuse addoper
 
 syntaxTree :: (Integer , Integer) -> Integer -> String -> String -> Bool -> Gen SynTree
 syntaxTree (minnode, maxnode) maxdepth lits minuse addoper
-    | maxdepth == 1 || maxnode == 1 = leafnode lits $ judgminuse minuse
-    | minnode == 1 && maxnode == 2 = oneof [ leafnode lits $ judgminuse minuse , negativeLiteral lits minuse]
+    | maxdepth == 1 || maxnode == 1 = leafNd
+    | minnode == 1 && maxnode == 2 = oneof [ leafNd , negativeLiteral lits minuse]
     | minnode == 2 && maxnode < 3 = negativeLiteral lits minuse
-    | minnode <= 2 && maxleafnode maxnode == toInteger (length minuse) = oneof $ binaryOper 3 addoper True
-    | maxleafnode maxnode == toInteger (length minuse) = oneof $ binaryOper minnode addoper True
-    | minnode == 2 && maxnode >= 3 = frequency [(1,negativeFormula (2, maxnode) maxdepth lits minuse addoper), (fromInteger (maxnode - minnode - 1), oneof $ binaryOper 3 addoper False)]
-    | minnode == 1 && maxnode >= 3 && length minuse <= 1 = frequency [(1,leafnode lits $ judgminuse minuse), (fromInteger (maxnode - minnode - 1), oneof ( negativeFormula (2, maxnode) maxdepth lits minuse addoper :  binaryOper 3 addoper False))]
-    | minnode == 1 && maxnode >= 3 && length minuse > 1= oneof ( negativeFormula (2, maxnode) maxdepth lits minuse addoper :  binaryOper 3 addoper False)
-    | (minnode-1) >= maxofnode ( maxdepth-1) = oneof $ binaryOper minnode addoper False
-    | otherwise = oneof ( negativeFormula (minnode, maxnode) maxdepth lits minuse addoper: binaryOper minnode addoper False)
+    | minnode <= 2 && maxleafnode maxnode == toInteger (length minuse) = oneof binaryOper
+    | maxleafnode maxnode == toInteger (length minuse) = oneof binaryOper
+    | minnode == 2 && maxnode >= 3 = frequency [(1, negativeForm), (fromInteger (maxnode - minnode - 1), oneof binaryOper)]
+    | minnode == 1 && maxnode >= 3 && length minuse <= 1 = frequency [(1,leafNd), (fromInteger (maxnode - minnode - 1), oneof ( negativeForm :  binaryOper))]
+    | minnode == 1 && maxnode >= 3 && length minuse > 1= oneof ( negativeForm :  binaryOper)
+    | (minnode-1) >= maxofnode ( maxdepth-1) = oneof  binaryOper
+    | otherwise = oneof ( negativeForm : binaryOper )
       where
-        binaryOper m adder choosodd = map (binaryOperator (m, maxnode) maxdepth lits minuse addoper choosodd) $ chooseList adder
+        binaryOper = map (binaryOperator (minnode, maxnode) maxdepth lits minuse addoper ) $ chooseList addoper
+        negativeForm = negativeFormula (minnode, maxnode) maxdepth lits minuse addoper
+        leafNd = leafnode lits $ judgminuse minuse
 
 chooseList :: Bool ->[SynTree -> SynTree -> SynTree]
 chooseList addoper = if addoper
     then [And, Or, Impl, Equi]
     else [And, Or]
 
-binaryOperator::(Integer , Integer) -> Integer -> String -> String -> Bool -> Bool -> (SynTree -> SynTree -> SynTree) -> Gen SynTree
-binaryOperator(minnode, maxnode) maxdepth lits minuse addoper choosodd oper = let a = maximum[0, minnode-1-maxofnode( maxdepth - 1 )]  in do
-    radmin <- if choosodd
-        then elements (filter odd [1+a..minnode-2-a])
-        else choose (1 + a,minnode - 2 - a)
-    radmax <- if choosodd
-        then elements (filter odd [radmin..maxnode-minnode+radmin])
-        else choose (radmin,maxnode - minnode + radmin)
-    left <- syntaxTree (radmin, radmax) (maxdepth - 1) lits (take (fromInteger $ maxleafnode radmax) minuse) addoper
-    right <- syntaxTree (minnode - radmin - 1, maxnode - radmax - 1) (maxdepth-1) lits (minuse \\  (take $ fromInteger(maxleafnode radmax)) minuse) addoper
-    return $ oper left right
+binaryOperator::(Integer , Integer) -> Integer -> String -> String ->  Bool -> (SynTree -> SynTree -> SynTree) -> Gen SynTree
+binaryOperator(minnode, maxnode) maxdepth lits minuse addoper  oper =
+    let avoidoveralloc = maximum[0, minnode-1-maxofnode( maxdepth - 1 )]
+        correctminnode = maximum [3,minnode]
+        chooseodd = maxleafnode maxnode == toInteger (length minuse)
+    in do
+      radmin <- if chooseodd
+        then elements (filter odd [1+avoidoveralloc..correctminnode - 2 - avoidoveralloc])
+        else choose (1 + avoidoveralloc,correctminnode - 2 - avoidoveralloc)
+      radmax <- if chooseodd
+        then elements (filter odd [radmin..maxnode - correctminnode + radmin])
+        else choose (radmin,maxnode - correctminnode + radmin)
+      left <- syntaxTree (radmin, radmax) (maxdepth - 1) lits (take (fromInteger $ maxleafnode radmax) minuse) addoper
+      right <- syntaxTree (correctminnode - radmin - 1, maxnode - radmax - 1) (maxdepth-1) lits (minuse \\  (take $ fromInteger(maxleafnode radmax)) minuse) addoper
+      return $ oper left right
 
 negativeFormula::(Integer , Integer) -> Integer -> String -> String -> Bool -> Gen SynTree
-negativeFormula (minnode, maxnode) maxdepth lits minus addoper = do
-    e <- syntaxTree (minnode - 1, maxnode - 1) (maxdepth - 1) lits minus addoper
-    return (Not e)
+negativeFormula (minnode, maxnode) maxdepth lits minus addoper =
+    let correctminnode = maximum [2,minnode]
+    in do
+      e <- syntaxTree (correctminnode - 1, maxnode - 1) (maxdepth - 1) lits minus addoper
+      return (Not e)
 
 negativeLiteral :: String -> String -> Gen SynTree
 negativeLiteral lits minus = do
