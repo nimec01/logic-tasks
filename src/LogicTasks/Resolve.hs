bfd693c71a7ecdb29bcd1444e521fddaf7a39e00
Patrick Ritzenfeld <patrick.ritzenfeld@stud.uni-due.de>
2022-07-25 21:11:15 +0200

    output monad for resolution task

--- src/LogicTasks/Resolve.hs
+++ src/LogicTasks/Resolve.hs
@@ -25,7 +25,6 @@ import Control.Monad.Output (
   translate
   )
 
-import Text.PrettyPrint.Leijen.Text
 
 
 
@@ -41,73 +40,77 @@ thrd3 (_,_,c) = c
 
 
 
-description :: ResolutionInst -> [ProxyDoc]
-description ResolutionInst{..} =
-              [ PMult ("Betrachten Sie die folgende Formel in KNF:"
-                      ,"Consider the following formula in cnf:"
-                      )
-              , PDoc line
-              , PDoc $ nest 4 $ pretty $ mkCnf clauses
-              , PDoc line
-              , PMult ("Führen Sie das Resolutionsverfahren an dieser Formel durch, um die leere Klausel abzuleiten."
-                      ,"Use the resolution technique on this formula to derive the empty clause."
-                      )
-              , PMult ("Geben Sie die Lösung als eine Liste von Tripeln an, wobei diese folgendermaßen aufgebaut sind: "
-                        ++ "(Erste Klausel, Zweite Klausel, Resolvente)"
-                      ,"Provide the solution as a list of triples with this structure: "
-                        ++ "(first clause, second clause, resolvent)."
-                      )
-              , PDoc line
-              , PMult ("Beachten Sie dabei für die ASCII-Formel diese Legende:"
-                      ,"Consider this key for the ASCII based formula:"
-                      )
-              , PDoc line
-              , Composite [ PMult ("Negation"
-                                  ,"negation"
-                                  )
-                          , PDoc $ myText ": ~"
-                          ]
-              , Composite [ PMult ("oder"
-                                  ,"or"
-                                  )
-                                  , PDoc $ myText ": \\/"
-                                  ]
-              , Composite [ PMult ("leere Klausel"
-                                  ,"empty clause"
-                                  )
-                                  , PDoc $ myText ": { }"
-                                  ]
-              , PDoc line
-              , PMult ("Optional können Sie Klauseln auch durch Nummern substituieren."
-                      ,"You can optionally substitute clauses with numbers."
-                      )
-              , PMult ("Klauseln aus der Formel sind bereits ihrer Reihenfolge nach nummeriert. (erste Klausel = 1, zweite Klausel = 2, ...)"
-                      ,"Clauses in the starting formula are already numbered by their order. (first clause = 1, second clause = 2, ...)"
-                      )
-              , PMult ("neu resolvierte Klauseln können mit einer Nummer versehen werden, indem Sie '= NUMMER' an diese anfügen."
-                      ,"Newly resolved clauses can be associated with a number by attaching '= NUMBER' behind them."
-                      )
-              , PDoc line
-              , Composite [ PMult ("Ein Lösungsversuch könnte beispielsweise so aussehen: "
-                                  , "A valid solution could look like this: ")
-                          , PDoc $ myText "[(1, 2, {A, ~B} = 5), (4, 5, { })]"
-                          ]
-              , PDoc line
-              , PDoc $ myText (fromMaybe "" addText)
-              ]
+description :: OutputMonad m => ResolutionInst -> LangM m
+description ResolutionInst{..} = do
+  paragraph $ translate $ do
+    german "Betrachten Sie die folgende Formel in KNF:"
+    english "Consider the following formula in cnf:"
+
+  -- PDoc $ nest 4 $ pretty $ mkCnf clauses
+
+  paragraph $ translate $ do
+    german "Führen Sie das Resolutionsverfahren an dieser Formel durch, um die leere Klausel abzuleiten."
+    english "Use the resolution technique on this formula to derive the empty clause."
+
+  paragraph $ translate $ do
+    german "Geben Sie die Lösung als eine Liste von Tripeln an, wobei diese folgendermaßen aufgebaut sind: (Erste Klausel, Zweite Klausel, Resolvente)"
+    english "Provide the solution as a list of triples with this structure: (first clause, second clause, resolvent)."
+
+  paragraph $ translate $ do
+    german "Beachten Sie dabei für die ASCII-Formel diese Legende:"
+    english "Consider this key for the ASCII based formula:"
+
+  paragraph $ do
+    translate $ do
+      german "Negation"
+      english "negation"
+    text ": ~"
+
+  paragraph $ do
+    translate $ do
+      german "oder"
+      english "or"
+    text ": \\/"
+
+  paragraph $ do
+    translate $ do
+      german "leere Klausel"
+      english "empty clause"
+    text ": { }"
+
+  paragraph $ translate $ do
+    german "Optional können Sie Klauseln auch durch Nummern substituieren."
+    english "You can optionally substitute clauses with numbers."
+
+  paragraph $ translate $ do
+    german "Klauseln aus der Formel sind bereits ihrer Reihenfolge nach nummeriert. (erste Klausel = 1, zweite Klausel = 2, ...)"
+    english "Clauses in the starting formula are already numbered by their order. (first clause = 1, second clause = 2, ...)"
+
+  paragraph $ translate $ do
+    german "neu resolvierte Klauseln können mit einer Nummer versehen werden, indem Sie '= NUMMER' an diese anfügen."
+    english "Newly resolved clauses can be associated with a number by attaching '= NUMBER' behind them."
+
+  paragraph $ do
+    translate $ do
+      german "Ein Lösungsversuch könnte beispielsweise so aussehen: "
+      english "A valid solution could look like this: "
+    text "[(1, 2, {A, ~B} = 5), (4, 5, { })]"
+
+  paragraph $ text (fromMaybe "" addText)
 
 
 
-verifyStatic :: ResolutionInst -> Maybe ProxyDoc
+verifyStatic :: OutputMonad m => ResolutionInst -> Maybe (LangM m)
 verifyStatic ResolutionInst{..}
     | any isEmptyClause clauses =
-        Just $ PMult ("Mindestens eine der Klauseln ist leer."
-                     ,"At least one of the clauses is empty."
-                     )
+        Just $ translate $ do
+          german "Mindestens eine der Klauseln ist leer."
+          english "At least one of the clauses is empty."
+
     | sat $ mkCnf clauses =
-        Just $ PMult ("Die Formel ist erfüllbar."
-                     ,"This formula is satisfiable."
-                     )
+        Just $ translate $ do
+          german "Die Formel ist erfüllbar."
+          english "This formula is satisfiable."
 
     | otherwise = Nothing
 
@@ -139,28 +142,28 @@ start = []
 
 
 
-partialGrade :: ResolutionInst -> [ResStep] -> Maybe ProxyDoc
+partialGrade :: OutputMonad m => ResolutionInst -> [ResStep] -> Maybe (LangM m)
 partialGrade ResolutionInst{..} sol
     | isJust checkMapping  = checkMapping
 
     | not (null wrongLitsSteps) =
-        Just $ Composite [ PMult ("Mindestens ein Schritt beinhaltet Literale, die in der Formel nicht vorkommen. "
-                                 ,"At least one step contains literals not found in the original formula. "
-                                 )
-                         , PDoc $ pretty wrongLitsSteps
-                         ]
+        Just $ paragraph $ do
+          translate $ do
+            german "Mindestens ein Schritt beinhaltet Literale, die in der Formel nicht vorkommen. "
+            english "At least one step contains literals not found in the original formula. "
+          itemizeM $ map (text . show) wrongLitsSteps
 
     | not (null noResolveSteps) =
-        Just $ Composite [ PMult ("Mindestens ein Schritt ist kein gültiger Resolutionsschritt. "
-                                 ,"At least one step is not a valid resolution step. "
-                                 )
-                         , PDoc $ pretty noResolveSteps
-                         ]
+        Just $ paragraph $ do
+          translate $ do
+            german "Mindestens ein Schritt ist kein gültiger Resolutionsschritt. "
+            english "At least one step is not a valid resolution step. "
+          itemizeM $ map (text . show) noResolveSteps
 
     | checkEmptyClause =
-        Just $ PMult ("Im letzten Schritt muss die leere Klausel abgeleitet werden."
-                     ,"The last step must derive the empty clause."
-                     )
+        Just $ translate $ do
+          german "Im letzten Schritt muss die leere Klausel abgeleitet werden."
+          english "The last step must derive the empty clause."
 
     | otherwise = Nothing
 
@@ -175,19 +178,18 @@ partialGrade ResolutionInst{..} sol
 
 
 
-completeGrade :: ResolutionInst -> [ResStep] -> Maybe ProxyDoc
+completeGrade :: OutputMonad m => ResolutionInst -> [ResStep] -> Maybe (LangM m)
 completeGrade ResolutionInst{..} sol =
     case applySteps clauses steps of
-        Nothing -> Just $ PMult ("In mindestens einem Schritt werden Klauseln resolviert, "
-                                  ++ "die nicht in der Formel sind oder noch nicht abgeleitet wurden."
-                                ,"In at least one step clauses are used, that are not part of the original formula "
-                                  ++ "and are not derived from previous steps."
-                                )
+        Nothing -> Just $ translate $ do
+                     german "In mindestens einem Schritt werden Klauseln resolviert, die nicht in der Formel sind oder noch nicht abgeleitet wurden."
+                     english "In at least one step clauses are used, that are not part of the original formula and are not derived from previous steps."
+
         Just solClauses -> if (any isEmptyClause solClauses)
                             then Nothing
-                            else Just $ PMult ("Die Leere Klausel wurde nicht korrekt abgeleitet."
-                                              ,"The Empty clause was not derived correctly."
-                                              )
+                            else Just $ translate $ do
+                                   german "Die Leere Klausel wurde nicht korrekt abgeleitet."
+                                   english "The Empty clause was not derived correctly."
 
       where
         steps = replaceAll sol $ baseMapping clauses
@@ -198,15 +200,17 @@ baseMapping :: [Clause] -> [(Int,Clause)]
 baseMapping xs = zip [1..] $ sort xs
 
 
-correctMapping :: [ResStep] -> [(Int,Clause)] -> Maybe ProxyDoc
+correctMapping :: OutputMonad m => [ResStep] -> [(Int,Clause)] -> Maybe (LangM m)
 correctMapping [] _ = Nothing
 correctMapping (Res (c1,c2,(c3,i)): rest) mapping
-    | checkIndices = Just $ PMult ("Mindestens ein Schritt verwendet einen nicht vergebenen Index. "
-                                  ,"At least one step is using an unknown index."
-                                  )
-    | alreadyUsed i = Just $ PMult ("Mindestens ein Schritt vergibt einen Index, welcher bereits verwendet wird. "
-                                   ,"At least one step assigns an index, which is already in use. "
-                                   )
+    | checkIndices = Just $ translate $ do
+                       german "Mindestens ein Schritt verwendet einen nicht vergebenen Index. "
+                       english "At least one step is using an unknown index."
+
+    | alreadyUsed i = Just $ translate $ do
+                        german "Mindestens ein Schritt vergibt einen Index, welcher bereits verwendet wird. "
+                        english "At least one step assigns an index, which is already in use. "
+
     | otherwise = correctMapping rest newMapping
 
 
