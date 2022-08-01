2b3a910031c53ba1e44438f0c80ea334e8336f77
xutongxi <49649765+xutongxi@users.noreply.github.com>
2022-08-01 16:05:51 +0200

    delete the possible to generate formula like A/\\((B\\/C)) (fmidue/ba-tongxi-xu#41)
    * delete the possible to generate (())

--- src/Tasks/LegalProposition/PrintBracket.hs
+++ src/Tasks/LegalProposition/PrintBracket.hs
@@ -2,15 +2,16 @@ module Tasks.LegalProposition.PrintBracket (
     bracketDisplay,
 ) where
 
-import Test.QuickCheck (Gen, frequency, elements)
+import Test.QuickCheck (Gen, elements)
 
-import Types (SynTree(..), treeNodes)
+import Types (SynTree(..),)
 import Print (normalShow)
 
 bracketDisplay :: SynTree Char -> Gen String
 bracketDisplay (And a b) = allocateBracketToSubtree False a b "/\\"
 bracketDisplay (Leaf a)=  return ("("++ (a : ")"))
 bracketDisplay (Or a b) = allocateBracketToSubtree False a b "\\/"
+bracketDisplay (Not (Leaf a)) = elements ["(~"++ (a : ")"), "~("++ (a : ")")]
 bracketDisplay (Not a) = do
     aFormula <- ifUsebracket True a
     return ('~' : aFormula)
@@ -18,11 +19,15 @@ bracketDisplay (Equi a b) = allocateBracketToSubtree False a b "<=>"
 bracketDisplay (Impl a b) = allocateBracketToSubtree False a b "=>"
 
 ifUsebracket :: Bool -> SynTree Char -> Gen String
+ifUsebracket usebracket (Leaf a) = if not usebracket then return [a] else return ['(', a, ')']
+ifUsebracket usebracket tree@(Not (Leaf a)) =
+    if not usebracket
+    then return (normalShow tree)
+    else elements ["(~"++ (a : ")"), "~("++ (a : ")")]
 ifUsebracket usebracket synTree =
-    let nodeNum = treeNodes synTree
-    in if not usebracket
-       then return (normalShow synTree)
-       else frequency [(1, implementBracket synTree), (fromIntegral nodeNum - 1, subTreebracket synTree)]
+    if not usebracket
+    then return (normalShow synTree)
+    else subTreebracket synTree
 
 subTreebracket :: SynTree Char -> Gen String
 subTreebracket (And a b) = allocateBracketToSubtree True a b "/\\"
@@ -42,14 +47,3 @@ allocateBracketToSubtree notFirstLayer a b usedOperator = do
     if notFirstLayer
     then return ("(" ++ left ++ usedOperator ++ right ++ ")")
     else return (left ++ usedOperator ++ right)
-
-bracketShow :: SynTree Char -> SynTree Char -> String -> Gen String
-bracketShow a b usedOperator = return ("((" ++ normalShow a ++ usedOperator ++ normalShow b ++ "))")
-
-implementBracket :: SynTree Char -> Gen String
-implementBracket (And a b) = bracketShow a b "/\\"
-implementBracket (Or a b) = bracketShow a b "\\/"
-implementBracket (Equi a b) = bracketShow a b "<=>"
-implementBracket (Impl a b) = bracketShow a b "=>"
-implementBracket (Not a) = return ("(~" ++ normalShow a ++ ")")
-implementBracket (Leaf a) = return ("(" ++ [a] ++ ")")
