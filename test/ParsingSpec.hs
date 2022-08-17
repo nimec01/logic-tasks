f226e3870df73f904c78c45062933e04a51b9a4e
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2022-08-17 13:24:53 +0200

    simple parser unit tests

--- test/ParsingSpec.hs
+++ test/ParsingSpec.hs
@@ -2,14 +2,25 @@ module ParsingSpec where
 
 import Trees.Parsing (formulaParse)
 import Tasks.LegalProposition.Parsing (illegalPropositionStringParse)
-import Data.Either (isRight)
+import Data.Either (isLeft, isRight)
+import Tasks.SuperfluousBrackets.Parsing (superfluousBracketsExcParser)
 
 import Test.Hspec ( describe, it, Spec)
 
 spec :: Spec
-spec =
+spec = do
   describe "formulaParse" $ do
     it "correctly recognizes simple double negation" $
       isRight $ formulaParse "~~A"
+    it "correctly rejects stuff that isn't strictly well-bracketed" $
+      isLeft $ formulaParse "A/\\B/\\C"
+    it "correctly rejects stuff with strange spaces" $
+      isLeft $ formulaParse "A/ \\B"
+  describe "illegalPropositionStringParse" $
     it "correctly recognizes simple set" $
       isRight $ illegalPropositionStringParse "{1,2,3,12,2}"
+  describe "superfluousBracketsExcParser" $ do
+    it "correctly recognizes stuff that isn't strictly well-bracketed" $
+      isRight $ superfluousBracketsExcParser "A/\\B/\\C"
+    it "correctly rejects stuff with strange spaces" $
+      isLeft $ superfluousBracketsExcParser "A/ \\B"
