eaaab0ec3286cc25cc9913dd57f8cfa7dfd5cb8c
xutongxi <49649765+xutongxi@users.noreply.github.com>
2022-08-24 13:01:55 +0200

    abstract

--- src/Tasks/LegalCNF/GenerateIllegal.hs
+++ src/Tasks/LegalCNF/GenerateIllegal.hs
@@ -82,7 +82,7 @@ genIllegalClauseShape ifFirstlayer ors = do
     else genIllegalShapeInSubTree ors (genIllegalClauseShape False) Or
 
 legalClauseShape :: Int -> SynTree BinOp ()
-legalClauseShape ors = foldr (Binary Or . Leaf) (Leaf ()) (replicate ors ())
+legalClauseShape ors = legalShape ors Or
 
 genIllegalCNFShape :: Int -> Gen (SynTree BinOp ())
 genIllegalCNFShape 0 = error "impossible"
@@ -94,7 +94,7 @@ genIllegalCNFShape ands = do
     else genIllegalShapeInSubTree ands genIllegalCNFShape And
 
 legalCNFShape :: Int -> SynTree BinOp ()
-legalCNFShape ands = foldr (Binary And . Leaf) (Leaf ()) (replicate ands ())
+legalCNFShape ands = legalShape ands And
 
 genIllegalOper :: (Int -> SynTree BinOp ()) -> [BinOp] -> Int -> Gen (SynTree BinOp ())
 genIllegalOper recF opers restOpers =
@@ -102,3 +102,6 @@ genIllegalOper recF opers restOpers =
         errorOper <- elements opers
         leftOpers <- choose (0, restOpers - 1)
         return (Binary errorOper (recF leftOpers) (recF (restOpers - 1 - leftOpers)))
+
+legalShape :: Int -> BinOp -> SynTree BinOp ()
+legalShape opers oper = foldr (Binary oper . Leaf) (Leaf ()) (replicate opers ())
