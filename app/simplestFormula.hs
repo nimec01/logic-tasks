c4551f82b69c314202f0e630804bde2a7ff2b58b
xutongxi <49649765+xutongxi@users.noreply.github.com>
2022-09-04 13:51:51 +0200

    Branch05 (fmidue/ba-tongxi-xu#73)
    Overall inspection of the thesis

--- app/simplestFormula.hs
+++ app/simplestFormula.hs
@@ -19,7 +19,7 @@ main = do
   putStrLn "\nThe following is a random instance generated from it:\n"
   inst@SuperfluousBracketsInst{ simplestString } <- generate . generateSuperfluousBracketsInst $ theConfigToUse
   pPrint inst
-  putStrLn "\n This is a important syntax task before you deal with CNF and DNF"
+  putStrLn "\n This is an important syntax task before you deal with CNF and DNF"
   putStrLn "\n Because of /\\ and \\/ are associative, it is not necessary to use brackets when combining three atoms with same operators /\\ or \\/ for example A/\\B/\\C"
   putStrLn "\n The task will give a formula with redundant brackets, and your mission is to give the simplest form of the formula"
   putStrLn "\n That means delete all unnecessary brackets"
