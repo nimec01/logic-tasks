83b751239ef1cabb9098fbc44491aa539d8ab349
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2023-02-08 17:18:10 +0100

    use existing parser for whitespace

--- src/ParsingHelpers.hs
+++ src/ParsingHelpers.hs
@@ -6,9 +6,9 @@ module ParsingHelpers (
   lexeme,
   ) where
 
-import Text.Parsec.Char (oneOf, satisfy)
-import Data.Char (isLetter, isSpace)
-import Text.Parsec (many, (<|>))
+import Text.Parsec.Char (oneOf, satisfy, spaces)
+import Data.Char (isLetter)
+import Text.Parsec ((<|>))
 import Text.Parsec.String (Parser)
 import Trees.Types (showOperator, showOperatorNot, allBinaryOperators)
 import Data.List.Extra (nubOrd)
@@ -17,9 +17,7 @@ formulaSymbol :: Parser Char
 formulaSymbol = satisfy isLetter <|> oneOf (nubOrd ("()" ++ showOperatorNot ++ concatMap showOperator allBinaryOperators))
 
 whitespace :: Parser ()
-whitespace = do
-    many $ satisfy isSpace
-    return ()
+whitespace = spaces
 
 lexeme :: Parser a -> Parser a
 lexeme p = do
