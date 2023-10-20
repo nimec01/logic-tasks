4096f99edaa62f7b94a15e196f993cf3268366b0
Oliver Westphal <oliver.westphal@uni-due.de>
2023-10-20 11:29:28 +0200

    add generator analysis code

--- src/LogicTasks/Debug.hs
+++ src/LogicTasks/Debug.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE LambdaCase #-}
 module LogicTasks.Debug where
 
 import Test.QuickCheck
@@ -8,6 +9,10 @@ import Control.Monad.Output
 import Text.Parsec
 import Text.Parsec.String (Parser)
 import Data.Maybe (isJust)
+import Formula.Types (Cnf(..), Clause(..), Literal(..), Formula(..))
+import Formula.Util (isPositive)
+import Data.Set (size, toList)
+import Data.List (partition, sort, nub)
 
 showDescription :: (m ~ GenericReportT Language (IO ()) IO) => Gen inst -> (inst -> LangM m) -> IO (Maybe ())
 showDescription gen f = do
@@ -47,3 +52,19 @@ run thing = do
   (r,sayThing) <- runLangMReport (pure ()) (>>) thing
   sayThing German
   pure r
+
+analyseCnfGenerator :: Gen Cnf -> IO ()
+analyseCnfGenerator gen = quickCheckWith stdArgs{maxSuccess=1000} $ forAll gen $ \cnf ->
+  tabulate "all literals" (map show $ literals cnf) $
+  tabulate "positive literals" (map show $ filter isPositive $ literals cnf) $
+  tabulate "negative literals" (map show $ filter (not . isPositive) $ literals cnf) $
+  tabulate "clause lengths" (map (show . size . literalSet) . toList $ clauseSet cnf) $
+  tabulate "number of clauses" (pure . show . size $ clauseSet cnf) $
+  tabulate "trivial clauses (containing both X and not X)" (map (show . isTrivial) . toList $ clauseSet cnf) $
+  tabulate "usage of atomic propositions" (pure . sort . nub . map (\case (Literal x) -> x ; (Not x) -> x) $ literals cnf)
+    True
+
+isTrivial :: Clause -> Bool
+isTrivial x =
+  let (pos,neg) = partition isPositive $ toList $ literalSet x
+  in any (\case (Literal a) -> Not a `elem` neg; _ -> error "impossible") pos
