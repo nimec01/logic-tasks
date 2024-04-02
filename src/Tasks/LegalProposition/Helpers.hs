9d51144416ec15c4b97495e053f4ef7206ff32a0
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2024-04-02 20:01:46 +0200

    simplify expression

--- src/Tasks/LegalProposition/Helpers.hs
+++ src/Tasks/LegalProposition/Helpers.hs
@@ -6,4 +6,4 @@ import Trees.Types (BinOp)
 formulaAmount :: SynTreeConfig -> Integer
 formulaAmount SynTreeConfig{..} =
   (if allowArrowOperators then fromIntegral (length [minBound..maxBound :: BinOp]) else 2)
-    ^ ((maxNodes - 1) `div` 2 - minUniqueBinOperators + 1)
+    ^ ((maxNodes + 1) `div` 2 - minUniqueBinOperators)
