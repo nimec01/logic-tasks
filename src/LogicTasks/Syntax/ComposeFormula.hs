a59790e32eb022e2d3e53071da50afb64efc0d0c
nimec01 <24428341+nimec01@users.noreply.github.com>
2024-10-02 14:23:15 +0200

    remove liftA2 import

--- src/LogicTasks/Syntax/ComposeFormula.hs
+++ src/LogicTasks/Syntax/ComposeFormula.hs
@@ -31,7 +31,6 @@ import UniversalParser (logicToken)
 import Text.Parsec (many, (<|>))
 import Data.Functor (void)
 import ParsingHelpers (tokenSymbol)
-import Control.Applicative (liftA2)
 import qualified Data.Map as Map (fromAscList)
 import Data.Traversable (for)
 
