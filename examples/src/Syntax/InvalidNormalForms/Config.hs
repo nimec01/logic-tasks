a8161cbcb57350e604abdb736c24422d9aeb9a7f
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2024-11-08 15:06:39 +0100

    weitere konkret gestellte Aufgaben

--- examples/src/Syntax/InvalidNormalForms/Config.hs
+++ examples/src/Syntax/InvalidNormalForms/Config.hs
@@ -1,4 +1,4 @@
-module Syntax.InvalidCnfs.Config where
+module Syntax.InvalidNormalForms.Config where
 
 import LogicTasks.Config (
   BaseConfig(..),
@@ -11,9 +11,9 @@ import Test.Hspec
 import Util.VerifyConfig
 import Control.OutputCapable.Blocks (Language(German))
 
--- Weight 0.33
-task07 :: LegalNormalFormConfig
-task07 = LegalNormalFormConfig
+-- 2024: Weight 0.25, used for CNFs
+task08 :: LegalNormalFormConfig
+task08 = LegalNormalFormConfig
   { normalFormConfig = NormalFormConfig
     { baseConf = BaseConfig
       { minClauseLength = 2, maxClauseLength = 4, usedLiterals = "ABCD" }
@@ -21,14 +21,34 @@ task07 = LegalNormalFormConfig
     , maxClauseAmount = 4
     }
   , formulas = 8
-  , illegals = 2
+  , illegals = 4
   , includeFormWithJustOneClause = True
   , includeFormWithJustOneLiteralPerClause = False
   , maxStringSize = 30
   , minStringSize = 12
   , allowArrowOperators = True
-  , extraText = Nothing
   , printSolution = True
+  , extraText = Nothing
+  }
+
+-- 2024: Weight 0.25, used for DNFs
+task09 :: LegalNormalFormConfig
+task09 = LegalNormalFormConfig
+  { normalFormConfig = NormalFormConfig
+    { baseConf = BaseConfig
+      { minClauseLength = 2, maxClauseLength = 4, usedLiterals = "ABCD" }
+    , minClauseAmount = 2
+    , maxClauseAmount = 4
+    }
+  , formulas = 8
+  , illegals = 3
+  , includeFormWithJustOneClause = False
+  , includeFormWithJustOneLiteralPerClause = True
+  , maxStringSize = 30
+  , minStringSize = 12
+  , allowArrowOperators = False
+  , printSolution = True
+  , extraText = Nothing
   }
 
 -- Weight 0.25
@@ -53,5 +73,6 @@ task18 = LegalNormalFormConfig
 
 spec :: Spec
 spec = do
-  describe "task07" $ verifyConfig German task07 checkLegalNormalFormConfig
+  describe "task08" $ verifyConfig German task08 checkLegalNormalFormConfig
+  describe "task09" $ verifyConfig German task09 checkLegalNormalFormConfig
   describe "task18" $ verifyConfig German task18 checkLegalNormalFormConfig
