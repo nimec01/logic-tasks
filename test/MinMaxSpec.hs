68607be9f06f86a3bc773a30bee12a7d86b3d58f
nimec01 <24428341+nimec01@users.noreply.github.com>
2025-02-27 17:48:14 +0100

    only use default config for Min and Max; also no grading checks

--- test/MinMaxSpec.hs
+++ test/MinMaxSpec.hs
@@ -10,66 +10,41 @@ import Config (dMinMaxConf, MaxInst (cnf), MinInst (dnf))
 
 
 
-
--- validBoundsMinMax :: Gen MinMaxConfig
--- validBoundsMinMax = do
---   cnfConf <- validBoundsNormalForm
---   let percentTrueEntries = Just (50,70)
---   pure $ MinMaxConfig
---     { cnfConf
---     , percentTrueEntries
---     , printSolution = False
---     , extraText = Nothing
---     , offerUnicodeInput = False
---     }
-
 spec :: Spec
 spec = do
   describe "config" $ do
     it "default config should pass config check" $
       doesNotRefuse (Max.verifyQuiz dMinMaxConf :: LangM Maybe) &&
       doesNotRefuse (Min.verifyQuiz dMinMaxConf :: LangM Maybe)
-    -- it "validBoundsMax should generate a valid config" $
-    --   forAll validBoundsMinMax $ \config ->
-    --     doesNotRefuse (Max.verifyQuiz config :: LangM Maybe) &&
-    --     doesNotRefuse (Min.verifyQuiz config :: LangM Maybe)
   describe "description" $ do
     it "should not reject - Max" $
-      -- forAll validBoundsMinMax $ \config ->
-        forAll (Max.genMaxInst dMinMaxConf) $ \inst ->
-          doesNotRefuse (Max.description inst :: LangM Maybe)
+      forAll (Max.genMaxInst dMinMaxConf) $ \inst ->
+        doesNotRefuse (Max.description inst :: LangM Maybe)
     it "should not reject - Min" $
-      -- forAll validBoundsMinMax $ \config ->
-        forAll (Min.genMinInst dMinMaxConf) $ \inst ->
-          doesNotRefuse (Min.description inst :: LangM Maybe)
+      forAll (Min.genMinInst dMinMaxConf) $ \inst ->
+        doesNotRefuse (Min.description inst :: LangM Maybe)
   describe "generateInst" $ do
     it "should pass verifyStatic - Max" $
-      -- forAll validBoundsMinMax $ \config ->
-        forAll (Max.genMaxInst dMinMaxConf) $ \inst ->
-          doesNotRefuse
-            (Max.verifyStatic inst :: LangM Maybe)
+      forAll (Max.genMaxInst dMinMaxConf) $ \inst ->
+        doesNotRefuse
+          (Max.verifyStatic inst :: LangM Maybe)
     it "should pass verifyStatic - Min" $
-      -- forAll validBoundsMinMax $ \config ->
-        forAll (Min.genMinInst dMinMaxConf) $ \inst ->
-          doesNotRefuse
-            (Min.verifyStatic inst :: LangM Maybe)
+      forAll (Min.genMinInst dMinMaxConf) $ \inst ->
+        doesNotRefuse
+          (Min.verifyStatic inst :: LangM Maybe)
     xit "possible solution passes partialGrade - Max" $
-      -- forAll validBoundsMinMax $ \config ->
-        forAll (Max.genMaxInst dMinMaxConf) $ \inst ->
-          doesNotRefuse
-            (Max.partialGrade' inst $ cnf inst :: LangM Maybe)
+      forAll (Max.genMaxInst dMinMaxConf) $ \inst ->
+        doesNotRefuse
+          (Max.partialGrade' inst $ cnf inst :: LangM Maybe)
     xit "possible solution passes partialGrade - Min" $
-      -- forAll validBoundsMinMax $ \config ->
-        forAll (Min.genMinInst dMinMaxConf) $ \inst ->
-          doesNotRefuse
-            (Min.partialGrade' inst $ dnf inst :: LangM Maybe)
-    it "possible solution passes completeGrade - Max" $
-      -- forAll validBoundsMinMax $ \config ->
-        forAll (Max.genMaxInst dMinMaxConf) $ \inst ->
-          doesNotRefuse
-            (Max.completeGrade' inst $ cnf inst :: LangM Maybe)
-    it "possible solution passes completeGrade - Min" $
-      -- forAll validBoundsMinMax $ \config ->
-        forAll (Min.genMinInst dMinMaxConf) $ \inst ->
-          doesNotRefuse
-            (Min.completeGrade' inst $ dnf inst :: LangM Maybe)
+      forAll (Min.genMinInst dMinMaxConf) $ \inst ->
+        doesNotRefuse
+          (Min.partialGrade' inst $ dnf inst :: LangM Maybe)
+    xit "possible solution passes completeGrade - Max" $
+      forAll (Max.genMaxInst dMinMaxConf) $ \inst ->
+        doesNotRefuse
+          (Max.completeGrade' inst $ cnf inst :: LangM Maybe)
+    xit "possible solution passes completeGrade - Min" $
+      forAll (Min.genMinInst dMinMaxConf) $ \inst ->
+        doesNotRefuse
+          (Min.completeGrade' inst $ dnf inst :: LangM Maybe)
